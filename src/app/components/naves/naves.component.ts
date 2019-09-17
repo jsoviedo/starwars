55b161cad1cee25b9d96fe18c45f72c09e2b13ce
Sebas <sebastyn16@gmail.com>
2019-09-17 08:39:50 -0500

    Cambie el color de la barra de navegacion, agregue la vista de las razas y creé el servicio para obtener los datos de la api para las naves

--- src/app/components/naves/naves.component.ts
+++ src/app/components/naves/naves.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { VechilesService } from 'src/app/services/vehicles.service';
 
 @Component({
   selector: 'app-naves',
   templateUrl: './naves.component.html',
-  styleUrls: ['./naves.component.css']
+  styleUrls: ['./naves.component.css'],
+  providers: [VechilesService]
 })
 export class NavesComponent implements OnInit {
 
-  constructor() { }
+  public vehicles: any;
+
+  constructor(
+    private _service: VechilesService 
+  ) { }
 
   ngOnInit() {
+    this._service.getVehicles().subscribe(
+
+      response => {
+        this.vehicles = response;
+        console.log(this.vehicles);
+      },
+
+      error => {
+        console.log(error);
+      }
+
+    );
   }
+
 
 }
---
+++ src/app/services/vehicles.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class VechilesService {
+
+    public url: string;
+
+    constructor(
+        private _http: HttpClient
+    ){
+        this.url = "https://swapi.co/";
+    }
+
+    getVehicles(){
+        return this._http.get(this.url + "api/vehicles/");
+    }
+}
