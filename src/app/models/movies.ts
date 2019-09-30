51734fb09f1c88954db1f9abcc9c6ea38c5d0f1c
javier hely medrano bermudez <medranobermudez@hotmail.com>
2019-09-30 10:34:46 -0500

    Creamos los servicios y el modelo para las peliculas

---
+++ src/app/models/movies.ts
@@ -0,0 +1,11 @@
+export class Starship {
+
+    constructor(
+        public name: string,
+        public model: string,
+        public manufacturer: string,
+        public crew: string,
+        public length: string,
+        public consumables: string,
+    ){
+}
