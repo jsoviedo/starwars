a1690c566b6ec9254cd8da99092d4ef20627c963
Sebas <sebastyn16@gmail.com>
2019-10-07 09:53:31 -0500

    Agregue el html para la vista del componente y el css, ademas arreglé los metodos del componente para hacer la consultas a la API

--- src/app/components/peliculas/peliculas.component.ts
+++ src/app/components/peliculas/peliculas.component.ts
@@ -26,8 +26,50 @@ export class PeliculasComponent implements OnInit {
    }
 
   ngOnInit() {
+    this._service.getMovies().subscribe(
+      response => {
+        let res: any;
+        res = response; // Recolecta la respuesta de la API en la variable 'res'
+        this.films = res.results; //Accede al arreglo 'results' que esta dentro de 'res' y lo guarda en films
+        this.getNames(); //Crea un arreglo con el nombre de las peliculas 
+        this.getMovie(2); //Por defecto inicia el componente con la pelicula que esta en la posicion '2' del arreglo 
+      },
+      error => {
+        console.log(error);
+      }
+    ); 
   }
 
+  getMovie(id: number){
+    try {
+      //Crea una nueva pelicula con el índice dado por parametro 
+      this.movie = new Movie(
+        this.films[id].title,
+        this.films[id].episode_id,
+        this.films[id].opening_crawl,
+        this.films[id].director,
+        this.films[id].producer,
+        this.films[id].release_date);
+    } catch(err) {
+      console.log(err);
+    }    
+  }
+  //Método para crear el arreglo de nombres
+  getNames(){
+    for(let i in this.films){
+      this.names.push(this.films[i].name); //Este método es util para buscar la información de una pelicula despues de que el usuario haya ingresado el nombre de una nave en la barra de búsqueda 
+    }
+  }
+
+  //Método para buscar la nave solicitada en la barra de búsqueda 
+  onFindMovie(name: string){
+    let index: number = this.names.indexOf(name); //Busca el indice de la nave solicitada en el arreglo de nombres 'names'
+    if(index === -1){ //Si el nombre de la nave no esta en en el arreglo de nombres, mostrará una alerta 
+      alert("Starship not found...");
+    } else {
+      this.getMovie(index); //Crea la nave que se mostrara en la vista 
+    }
+  }
 }
 
 
@@ -39,52 +81,4 @@ export class PeliculasComponent implements OnInit {
   
    
     
-  }
 
-  ngOnInit() {
-    this._service.getStarships().subscribe(
-      response => {
-        let res: any;
-        res = response; // Recolecta la respuesta de la API en la variable 'res'
-        this.starships = res.results; //Accede al arreglo 'results' que esta dentro de 'res' y lo guarda en starships
-        this.getNames(); //Crea un arreglo con el nombre de las naves 
-        this.getStarship(2); //Por defecto inicia el componente con la nave que esta en la posicion '2' del arreglo 
-      },
-      error => {
-        console.log(error);
-      }
-    ); 
-  }
-
-  getStarship(id: number){
-    try {
-      //Crea una nueva nave con el índice dado por parametro 
-      this.starship = new Starship(
-        this.starships[id].name,
-        this.starships[id].model,
-        this.starships[id].manufacturer,
-        this.starships[id].crew,
-        this.starships[id].length,
-        this.starships[id].consumables);
-    } catch(err) {
-      console.log(err);
-    }    
-  }
-  
-  //Método para buscar la nave solicitada en la barra de búsqueda 
-  onFindStarship(name: string){
-    let index: number = this.names.indexOf(name); //Busca el indice de la nave solicitada en el arreglo de nombres 'names'
-    if(index === -1){ //Si el nombre de la nave no esta en en el arreglo de nombres, mostrará una alerta 
-      alert("Starship not found...");
-    } else {
-      this.getStarship(index); //Crea la nave que se mostrara en la vista 
-    }
-  }
-
-  //Método para crear el arreglo de nombres
-  getNames(){
-    for(let i in this.starships){
-      this.names.push(this.starships[i].name); //Este método es util para buscar la información de una nave despues de que el usuario haya ingresado el nombre de una nave en la barra de búsqueda 
-    }
-  }
-}
