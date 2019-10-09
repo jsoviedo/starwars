d7329704c44f235d26e200ad3bb6b25112e34e45
Jutinoco <55327297+Jutinoco@users.noreply.github.com>
2019-10-09 09:19:35 -0500

    Se crean los atributos correspondientes
    Atributos que seran extraidos del API

---
+++ src/app/models/starpeople.ts
@@ -0,0 +1,12 @@
+export class Starpeople {
+
+    // Modelo para las naves
+    constructor(
+        public name: string,
+        public height: height,
+        public skin_color: skin_color,
+        public birth_year: birth_year,
+        public mass: mass,
+        public gender: gender,
+    ){}
+}
